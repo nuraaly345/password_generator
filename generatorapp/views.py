73be0b2ad2483e151b622c0b5ab20d909760f283
nuraaly345 <kubanura57@gmail.com>
2022-07-12 19:28:01 +0300

    Тамыр папкадагы urls.py каттам файлын generatorapp папкасындагы views.py көрүнүштөр файлына home жана generator функциясын аныктоо

--- generatorapp/views.py
+++ generatorapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Салам, бул сайттын биринчи барагы")
+
+def generator(request):
+    return HttpResponse("Сыр сөздү жаратуу барагы")
--- pass_generator/urls.py
+++ pass_generator/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generatorapp.views import home, generator
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('', home),
+    path('generator/', generator),
+    ]
