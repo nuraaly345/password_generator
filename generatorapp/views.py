285e0955e5a19e5142a9e417d916984d447cd3e7
nuraaly345 <kubanura57@gmail.com>
2022-07-13 16:50:06 +0300

    Сыр создү жаратуучу функция түзүү

--- generatorapp/views.py
+++ generatorapp/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 def home(request):
-    return render(request, 'generatorapp/home.html')
+    return render(request, 'base.html')
 
 def generator(request):
-    return HttpResponse("Сыр сөздү жаратуу барагы")
+
+    alphabet = list('abcdefghiklmnopqrstvxyz')
+    number = 10
+    password = ''
+
+    for i in range(number):
+        password+= random.choice(alphabet)
+
+    return render(request, 'generatorapp/home.html', {'password': password})
